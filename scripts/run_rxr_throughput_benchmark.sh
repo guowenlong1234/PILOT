ca285bc7d110a53395bcd03db7909d76a7112236
gwl <g18539225688@gmail.com>
2026-09-20 15:49:50 +0800

    Bound compiler CPU parallelism during dual-rank visual benchmarks

--- scripts/run_rxr_throughput_benchmark.sh
+++ scripts/run_rxr_throughput_benchmark.sh
@@ -8,5 +8,6 @@ export ETPR1_BENCH_HABITAT_BASELINES_ROOT="$runtime_root/habitat-baselines/habit
 export LD_PRELOAD=/lib/x86_64-linux-gnu/libGLdispatch.so.0
 export GLOG_minloglevel=2 MAGNUM_LOG=quiet HABITAT_SIM_LOG=quiet
 export OMP_NUM_THREADS=1
+export TORCHINDUCTOR_COMPILE_THREADS=2
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True,garbage_collection_threshold:0.8
 exec /home/gwl/miniconda3/envs/etpnav_unified/bin/torchrun --nproc_per_node=2 --master_port=24693 scripts/benchmark_rxr_throughput.py "$@"
